ae92f0b99e36aa8aa6a3c4959a7900111acd83c8
wangshengnan <wangshengnan@shdatagroup.com>
2025-10-14 17:12:09 +0800

    Update Website: test vue-qrcode-reader

---
+++ assets/usePageOptions.BTEaFE2L.js
@@ -0,0 +1,1 @@
+import{aO as o,W as n}from"./index-DbmF-9aG.js";function s(s){const a=o();n(()=>{var o;const n=null==(o=a.value.$vm)?void 0:o.options;s(n)})}export{s as u};
--- assets/usePageOptions.aM0KPUm6.js
+++
@@ -1,1 +0,0 @@
-import{aQ as o,W as n}from"./index-BAYT1_GE.js";function s(s){const a=o();n(()=>{var o;const n=null==(o=a.value.$vm)?void 0:o.options;s(n)})}export{s as u};
